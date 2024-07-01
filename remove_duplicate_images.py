5a45e59da95922e7ac657496844cb541e84d0cbc
jMujunen <jMujunen@github.com>
2024-07-01 13:58:25 -0700

    Fix linting and formatting
    - Remove unused imports and fixed star (*) imports
    - Linted with ruff (single to double quotes)

--- remove_duplicate_images.py
+++ remove_duplicate_images.py
@@ -3,16 +3,12 @@
 # remove_duplicate_media.py - Finds and removes duplicate images and videos.
 
 import os
-import sys
-from time import sleep
-import re
-import shutil
 import argparse
-
+import subprocess
 from ExecutionTimer import ExecutionTimer
 from ProgressBar import ProgressBar
-from MetaData import *
-from Color import *
+from fsutils import Dir
+from Color import cprint, fg, style
 
 IGNORED_DIRS = [".Trash-1000", "Screenshots", "RuneLite"]
 
@@ -31,7 +27,6 @@ def parse_args():
 
 
 def main(args):
-
     with ExecutionTimer():
         hashes = {}
         duplicate_files = []
@@ -42,17 +37,13 @@ def main(args):
         progress = ProgressBar(files)
         try:
             for item in directory:
-                if (
-                    "Trash-1000" in item.path
-                    or "RuneLite" in item.path
-                    or "Wallpaper" in item.path
-                ):
+                if "Trash-1000" in item.path or "RuneLite" in item.path or "Wallpaper" in item.path:
                     continue
 
                 if item.is_file:
                     progress.increment()
                 if item.is_file and item.is_image:
-                    hash = item.calculate_hash('dhash')
+                    hash = item.calculate_hash("dhash")
                     if hash in hashes.keys():
                         hashes[hash].append(item.path)
                         cprint(
@@ -75,7 +66,7 @@ def main(args):
                     )
                     duplicate_files.append(img)
 
-                reply = input(f"\033[33mRemove these files? [Y/n]: \033[0m")
+                reply = input("\033[33mRemove these files? [Y/n]: \033[0m")
                 if reply.lower() == "y" or reply == "":
                     for i, img in enumerate(v):
                         # Skip the first image
